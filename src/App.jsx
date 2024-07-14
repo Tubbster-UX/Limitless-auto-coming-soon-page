81c81d90d8639ae413a8ba86e4c116f231933ee8
Tubbster-UX <Alexavierbrink@gmail.com>
2024-07-14 17:51:35 -0600

    chore: Add Vite configuration and setup React app structure

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <img src='Limitless Auto.png' />
+      <h1>Coming Soon</h1>
+    </div>
+  )
+}
+
+export default App
