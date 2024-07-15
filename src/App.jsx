78b44b45e93a78a015c94f4e9d4e6a00a11d6ec2
Tubbster-UX <Alexavierbrink@gmail.com>
2024-07-14 18:10:37 -0600

    chore: Add Tailwind CSS configuration and update app styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div>
-      <img src='Limitless Auto.png' />
-      <h1>Coming Soon</h1>
+    <div class="flex flex-row min-h-screen justify-center items-center bg-black">
+      <div className="w-full sm:w-2/3 md:w-1/2 lg:w-1/3 flex flex-col justify-center text-center space-y-10 sm:space-y-20 md:space-y-30">
+        <img src='Limitless Auto.png' className="w-3/4 mx-auto" />
+        <h1 className="text-xl sm:text-2xl md:text-2xl lg:text-4xl font-black text-white">COMING SOON</h1>
+      </div>
     </div>
   )
 }
